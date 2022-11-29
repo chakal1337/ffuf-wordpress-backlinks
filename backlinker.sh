e9ec732740833e94ab1d43e15b755bee82f72a30
chakal <54026816+chakal1337@users.noreply.github.com>
2022-11-29 17:11:35 -0500

    Update backlinker.sh

--- backlinker.sh
+++ backlinker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [[ $# < 2 ]]; then
- echo "$0 <url> <blog list>";
+if [[ $# < 3 ]]; then
+ echo "$0 <url> <blog list> <threads>";
  exit;
 fi
-ffuf -u FUZZ -w $2 -H "Content-Type: application/x-www-form-urlencoded" -H "Referer: $1" -d "comment=$1&author=hey&email=hey%40gmail.com&url=$1&submit=Post+Comment&comment_post_ID=5&comment_parent=0" -H "User-Agent: Mozilla/5.0 (Firefox)"
+ffuf -t $3 -u FUZZwp-comments-post.php -w $2 -H "Content-Type: application/x-www-form-urlencoded" -H "Referer: $1" -d "comment=$1&author=hey&email=hey%40gmail.com&url=$1&submit=Post+Comment&comment_post_ID=5&comment_parent=0" -H "User-Agent: Mozilla/5.0 (Firefox)" -mc 200
